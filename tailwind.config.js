1138c4c82c84f62312b5536efd9065a324a4fa63
Miki Pejic <Miki.Pejic@matchesfashion.com>
2024-12-25 11:58:19 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JSX/JS files in your src folder
+    './public/index.html', // Include the HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
